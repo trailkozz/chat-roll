3c1bfe712f459c063a64771279fa4389af9371a9
trailkozz <kozz_mo@yahoo.com>
2023-05-29 20:59:36 -0700

    Updated to include dice rolls and remove location

--- public/js/chat.js
+++ public/js/chat.js
@@ -1,118 +1,115 @@
-const socket = io()
-const $msgForm = document.querySelector("#user")
-const $msgForInput = $msgForm.querySelector('input')
-const $msgFormButton = $msgForm.querySelector('button')
+const socket = io();
+const $msgForm = document.querySelector("#user");
+const $msgForInput = $msgForm.querySelector("input");
+const $msgFormButton = $msgForm.querySelector("button");
 // const $msgForInput = $msgForm.querySelector('input')
-const $geoLocationButton = document.querySelector("#getLocation")
-const $messages = document.querySelector("#messages")
-const $locations = document.querySelector("#locations")
+const $roll3d6Button = document.querySelector("#roll3d6");
+const $roll1d6Button = document.querySelector("#roll1d6");
+const $messages = document.querySelector("#messages");
+const $locations = document.querySelector("#locations");
 
-const msgtemplate = document.querySelector("#msg-template").innerHTML
-const loctemplate = document.querySelector("#location-template").innerHTML
-const sidebartemplate = document.querySelector('#user-rooms').innerHTML
+const msgtemplate = document.querySelector("#msg-template").innerHTML;
+const rolltemplate = document.querySelector("#roll-template").innerHTML;
+const sidebartemplate = document.querySelector("#user-rooms").innerHTML;
 
-const { username, room } = Qs.parse(location.search, { ignoreQueryPrefix: true })
+const { username, room } = Qs.parse(location.search, {
+  ignoreQueryPrefix: true,
+});
 
 const autoscroll = () => {
-    const $newmsg = $messages.lastElementChild
-    const newmsgStyles = getComputedStyle($newmsg)
+  const $newmsg = $messages.lastElementChild;
+  const newmsgStyles = getComputedStyle($newmsg);
 
-    const newMsgMargin = parseInt(newmsgStyles.marginBottom)
-    const newmsgheight = $newmsg.offsetHeight + newMsgMargin
+  const newMsgMargin = parseInt(newmsgStyles.marginBottom);
+  const newmsgheight = $newmsg.offsetHeight + newMsgMargin;
 
+  const visibleHeight = $messages.offsetHeight;
 
+  const containerHeght = $messages.scrollHeight;
 
-    const visibleHeight = $messages.offsetHeight
+  const scrollOffset = $messages.scrollTop + visibleHeight;
 
-    const containerHeght = $messages.scrollHeight
-
-    const scrollOffset = $messages.scrollTop + visibleHeight
-
-    if (containerHeght - newmsgheight <= scrollOffset) {
-        $messages.scrollTop = $messages.scrollHeight
-
-    }
-}
+  if (containerHeght - newmsgheight <= scrollOffset) {
+    $messages.scrollTop = $messages.scrollHeight;
+  }
+};
 socket.on("message", (msg) => {
-    console.log(msg)
-    const html = Mustache.render(msgtemplate, {
-        username: msg.username,
-        msg: msg.text,
-        createdAt: moment(msg.createdAt).format('h:m A, DD MMM,YYYY')
-    })
+  console.log(msg);
+  const html = Mustache.render(msgtemplate, {
+    username: msg.username,
+    msg: msg.text,
+    createdAt: moment(msg.createdAt).format("h:m A, DD MMM,YYYY"),
+  });
 
-    $messages.insertAdjacentHTML("beforeend", html)
-    autoscroll()
-})
+  $messages.insertAdjacentHTML("beforeend", html);
+  autoscroll();
+});
 
+socket.on("rolld6", (msg) => {
+  console.log(msg);
+  const html = Mustache.render(rolltemplate, {
+    username: msg.username,
+    msg: msg.text,
+    createdAt: moment(msg.createdAt).format("h:m A, DD MMM, YYYY"),
+  });
 
-socket.on("locationurl", (loc) => {
-    console.log(loc)
-    const html = Mustache.render(loctemplate, {
-        username: loc.username,
-        loc: loc.url,
-        createdAt: moment(loc.createdAt).format('h:m A, DD MMM,YYYY')
-
-    })
-
-    $messages.insertAdjacentHTML("beforeend", html)
-    autoscroll()
-})
+  $messages.insertAdjacentHTML("beforeend", html);
+  autoscroll();
+});
 
 socket.on("roomData", ({ room, users }) => {
-    const html = Mustache.render(sidebartemplate, {
-        room,
-        users
-    })
-    document.querySelector('#sidebar').innerHTML = html
-})
+  const html = Mustache.render(sidebartemplate, {
+    room,
+    users,
+  });
+  document.querySelector("#sidebar").innerHTML = html;
+});
 
 $msgForm.addEventListener("submit", (e) => {
-    e.preventDefault()
+  e.preventDefault();
 
-    $msgFormButton.setAttribute('disabled', 'disabled')
-    let msg = document.querySelector("input").value
+  $msgFormButton.setAttribute("disabled", "disabled");
+  let msg = document.querySelector("input").value;
 
-    socket.emit("sendMessage", msg, (error) => {
-        $msgFormButton.removeAttribute('disabled')
-        $msgForInput.value = ''
-        $msgForInput.focus()
-        if (error) {
-            return console.log(error)
-        }
-
-        console.log('Message delivered!')
-    })
-})
-
-// const $msgForm=document.querySelector("#user") 
-
-
-$geoLocationButton.addEventListener("click", () => {
-    if (!navigator.geolocation) {
-        return alert("no browser supoport")
-    }
-
-    $geoLocationButton.setAttribute('disabled', 'disabled')
-    navigator.geolocation.getCurrentPosition((position) => {
-
-        socket.emit("sendLocation", {
-            latitude: position.coords.latitude,
-            longitude: position.coords.longitude,
-        }, () => {
-            $msgForInput.focus()
-            $geoLocationButton.removeAttribute('disabled')
-            console.log("location shared", position)
-        })
-
-    })
-})
-
-socket.emit('join', { username, room }, (error) => {
+  socket.emit("sendMessage", msg, (error) => {
+    $msgFormButton.removeAttribute("disabled");
+    $msgForInput.value = "";
+    $msgForInput.focus();
     if (error) {
-        alert(error)
-        location.href = '/'
+      return console.log(error);
     }
-})
 
+    console.log("Message delivered!");
+  });
+});
 
+$roll3d6Button.addEventListener("click", () => {
+  $roll3d6Button.setAttribute("disabled", "disabled");
+  socket.emit("roll3d6", "", (error) => {
+    if (error) {
+      return console.log(error);
+    }
+    console.log("Rolled successfully!");
+  });
+  $roll3d6Button.removeAttribute("disabled");
+});
+
+$roll1d6Button.addEventListener("click", () => {
+  $roll1d6Button.setAttribute("disabled", "disabled");
+  socket.emit("roll1d6", "", (error) => {
+    if (error) {
+      return console.log(error);
+    }
+    console.log("Rolled successfully!");
+  });
+  $roll1d6Button.removeAttribute("disabled");
+});
+
+// const $msgForm=document.querySelector("#user")
+
+socket.emit("join", { username, room }, (error) => {
+  if (error) {
+    alert(error);
+    location.href = "/";
+  }
+});
--- src/index.js
+++ src/index.js
@@ -1,73 +1,105 @@
-const path = require('path')
-const http = require('http')
-const express = require('express')
-const socketio = require('socket.io')
-const { generatemsg, generateLocation } = require('./utils/messages')
+const path = require("path");
+const http = require("http");
+const express = require("express");
+const socketio = require("socket.io");
+const { generatemsg, generateLocation } = require("./utils/messages");
 
-const { addUser, removeUser, getUser, getUserInRoom } = require('./utils/users')
+const {
+  addUser,
+  removeUser,
+  getUser,
+  getUserInRoom,
+} = require("./utils/users");
 
+const app = express();
+const server = http.createServer(app);
+const io = socketio(server);
 
-const app = express()
-const server = http.createServer(app)
-const io = socketio(server)
+const PORT = process.env.PORT || 3000;
 
-const PORT = process.env.PORT || 3000
+const publicdir = path.join(__dirname, "../public");
 
-const publicdir = path.join(__dirname, '../public')
-
-app.use(express.static(publicdir))
-
+app.use(express.static(publicdir));
 
 io.on("connection", (socket) => {
-    console.log("new connection")
+  console.log("new connection");
 
-    socket.on("join", ({ username, room }, cb) => {
+  socket.on("join", ({ username, room }, cb) => {
+    const { error, user } = addUser({ id: socket.id, username, room });
 
-        const { error, user } = addUser({ id: socket.id, username, room })
+    if (error) {
+      return cb(error);
+    }
+    socket.join(user.room);
+    socket.emit(
+      "message",
+      generatemsg(`Welcome, ${user.username}, to room ${user.room}!`)
+    );
+    socket.broadcast
+      .to(user.room)
+      .emit("message", generatemsg(`Player ${user.username} has joined!`));
 
-        if (error) {
-            return cb(error)
-        }
-        socket.join(user.room)
-        socket.emit("message", generatemsg("Admin ,Welcome"))
-        socket.broadcast.to(user.room).emit("message", generatemsg(`Admin ${user.username} has joined!`))
+    io.to(user.room).emit("roomData", {
+      room: user.room,
+      users: getUserInRoom(user.room),
+    });
+    cb();
+  });
 
-        io.to(user.room).emit("roomData", {
-            room: user.room,
-            users: getUserInRoom(user.room)
-        })
-        cb()
-    })
+  socket.on("sendMessage", (msg, cb) => {
+    const user = getUser(socket.id);
+    io.to(user.room).emit("message", generatemsg(user.username, msg));
+    cb();
+  });
 
-    socket.on("sendMessage", (msg, cb) => {
-        const user = getUser(socket.id)
-        io.to(user.room).emit("message", generatemsg(user.username, msg))
-        cb()
-    })
+  socket.on("roll3d6", (msg, cb) => {
+    const user = getUser(socket.id);
+    var dieRolls = [];
+    for (var i = 0; i < 3; i++) {
+      dieRolls.push(Math.round(Math.random() * 5 + 1));
+    }
+    io.to(user.room).emit(
+      "message",
+      generatemsg(
+        "Rollbot",
+        `${user.username} rolled a total of ${dieRolls.reduce(
+          (a, b) => a + b
+        )}.  Details: [${dieRolls}]`
+      )
+    );
+  });
 
-    socket.on("sendLocation", (location, cb) => {
-        const user = getUser(socket.id)
-        console.log(user)
-        io.to(user.room).emit("locationurl", generateLocation(user.username, `https://www.google.com/maps?q=${location.latitude},${location.longitude}`))
-        cb()
-    })
+  socket.on("roll1d6", (msg, cb) => {
+    const user = getUser(socket.id);
+    var dieRolls = [];
+    for (var i = 0; i < 3; i++) {
+      dieRolls.push(Math.round(Math.random() * 5 + 1));
+    }
+    io.to(user.room).emit(
+      "message",
+      generatemsg(
+        "Rollbot",
+        `${user.username} rolled a ${Math.round(Math.random() * 5) + 1}`
+      )
+    );
+  });
 
-    socket.on("disconnect", () => {
-        const user = removeUser(socket.id)
-        console.log(user)
-        if (user) {
-            io.to(user.room).emit("message", generatemsg(`Admin ${user.username} A user  has left`))
+  socket.on("disconnect", () => {
+    const user = removeUser(socket.id);
+    console.log(user);
+    if (user) {
+      io.to(user.room).emit(
+        "message",
+        generatemsg(`Player ${user.username} has left`)
+      );
 
-            io.to(user.room).emit("roomData", {
-                room: user.room,
-                users: getUserInRoom(user.room)
-            })
-        }
-
-    })
-
-
-})
+      io.to(user.room).emit("roomData", {
+        room: user.room,
+        users: getUserInRoom(user.room),
+      });
+    }
+  });
+});
 server.listen(PORT, () => {
-    console.log("server s up" + PORT)
-})
+  console.log("server s up" + PORT);
+});
